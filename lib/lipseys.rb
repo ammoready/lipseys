3e23c3f80a5968e0405445421f5cec0afeac843f
Dale Campbell <oshuma@gmail.com>
2016-11-11 15:57:56 -0500

    Adding order submit support.

--- lib/lipseys.rb
+++ lib/lipseys.rb
@@ -2,10 +2,12 @@ require 'lipseys/version'
 
 require 'net/http'
 require 'nokogiri'
+require 'savon'
 
 require 'lipseys/base'
 require 'lipseys/catalog'
 require 'lipseys/inventory'
+require 'lipseys/order'
 
 module Lipseys
   class NotAuthenticated < StandardError; end
---
+++ lib/lipseys/order.rb
@@ -0,0 +1,68 @@
+module Lipseys
+  class Order < Base
+
+    API_URL = 'http://www.lipseys.com/webservice/LipseyAPI.asmx?WSDL'
+
+
+    def initialize(options = {})
+      requires!(options, :email, :password, :quantity)
+      @email = options[:email]
+      @password = options[:password]
+      @quantity = options[:quantity]
+
+      @notify_by_email = (options[:notify_by_email] == true ? 'Y' : nil)
+      @po_number = options[:po_number]
+      @note = options[:note]
+
+      @item_number_or_upc = options[:item_number] || options[:upc]
+      raise ArgumentError.new("Either :item_number or :upc must be given") if @item_number_or_upc.nil?
+    end
+
+
+    def self.submit(*args)
+      new(*args).submit
+    end
+
+    def submit
+      client = Savon.client(wsdl: API_URL, convert_request_keys_to: :none)
+      response = client.call(:submit_order, message: build_order_data)
+
+      raise Lipseys::NotAuthenticated if not_authenticated?(response)
+
+      order_result = response.body[:submit_order_response][:submit_order_result]
+
+      {
+        order_number: order_result[:order_no],
+        new_order: (order_result[:new_order] == 'Y'),
+        success: (order_result[:success] == 'Y'),
+        description: order_result[:return_desc],
+        quantity_received: Integer(order_result[:qty_received]),
+      }
+    rescue Savon::Error => e
+      { success: false, description: e.to_s }
+    end
+
+    private
+
+    def build_order_data
+      order_data = {
+        EmailAddress: @email,
+        Password: @password,
+        LipseysItemNumberOrUPC: @item_number_or_upc,
+        Qty: @quantity
+      }
+
+      order_data[:NotifyByEmail] = @notify_by_email unless @notify_by_email.nil?
+      order_data[:PONumber] = @po_number unless @po_number.nil?
+      order_data[:Note] = @note unless @note.nil?
+
+      { OrderData: order_data }
+    end
+
+    def not_authenticated?(response)
+      order_result = response.body[:submit_order_response][:submit_order_result]
+      order_result[:success] == 'N' && order_result[:return_desc] =~ /Credentials Not Valid/i
+    end
+
+  end
+end
