fc71901e2794b6511bf575644924382150f1e8aa
Justin Wilson <justin@jwils.co>
2018-01-05 16:04:53 -0500

    Prettier names

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+require 'webmock/rspec'
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "lipseys"
+
+# Require all files from the /spec/support dir
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include Mocker
+end
