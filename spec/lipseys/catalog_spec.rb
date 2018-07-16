9c306e99dde234606ecd2bafb87b7c81ea4f9483
Justin Wilson <justin@jwils.co>
2018-07-16 14:30:51 -0400

    Refactor catalog class to yield item rather than chunk
    #4

--- spec/lipseys/catalog_spec.rb
+++ spec/lipseys/catalog_spec.rb
@@ -2,30 +2,34 @@ require "spec_helper"
 
 describe Lipseys::Catalog do
 
+  let(:default_headers) { { 'Accept' => '*/*', 'Host' => 'www.lipseys.com' } }
   let(:options) { { username: '123', password: 'abc' } }
 
   before do
     stub_request(:get, "https://www.lipseys.com/API/catalog.ashx?email=123&pass=abc").
-      with(headers: default_headers).to_return(status: 200, body: sample_catalog_response)
+      with(headers: default_headers).to_return(status: 200, body: FixtureHelper.get_fixture_file('LipseysCatalog.xml').read)
 
     stub_request(:get, "https://www.lipseys.com/API/pricequantitycatalog.ashx?email=123&pass=abc").
-      with(headers: default_headers).to_return(status: 200, body: sample_inventory_response)
+      with(headers: default_headers).to_return(status: 200, body: FixtureHelper.get_fixture_file('LipseysInventoryPricing.xml').read)
   end
 
   describe '.all' do
-    it 'Returns a hash of data' do
-      Lipseys::Catalog.all(10, options) do |chunk|
-        chunk.each_with_index do |item, index|
-          case index
-          when 0
-            expect(item[:upc]).to  eq('082442188744')
-            expect(item[:name]).to eq('21 Bobcat J212500')
-          when 1
-            expect(item[:upc]).to  eq('082442188812')
-            expect(item[:name]).to eq('3032 Tomcat J320500')
-          end
+    it 'Yields each item to a block' do
+      count = 0
+
+      Lipseys::Catalog.all(options) do |item|
+        count += 1
+        case count
+        when 1
+          expect(item[:upc]).to  eq('968000000001')
+          expect(item[:name]).to eq('A00111 TWIYO A00111')
+        when 2
+          expect(item[:upc]).to  eq('968000000002')
+          expect(item[:name]).to eq('A00112 RHYZIO A00112')
         end
       end
+
+      expect(count).to eq(44)
     end
   end
 
