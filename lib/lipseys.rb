ae9a03e6c028c7a0fc14f5d4ff89cb39246fd4e9
Jeffrey Dill <jeffdill2@gmail.com>
2020-03-29 16:49:16 -0400

    Remove no longer needed components #711

--- lib/lipseys.rb
+++ lib/lipseys.rb
@@ -1,20 +1,10 @@
 require 'lipseys/version'
 
-require 'net/http'
-require 'nokogiri'
-require 'savon'
 
 require 'lipseys/base'
-require 'lipseys/soap_client'
 
 require 'lipseys/catalog'
 require 'lipseys/inventory'
-require 'lipseys/invoice'
 require 'lipseys/order'
 require 'lipseys/user'
 require 'lipseys/image'
-require 'lipseys/parser'
-
-module Lipseys
-  class NotAuthenticated < StandardError; end
-end
