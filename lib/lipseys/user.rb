94bb225ee2e72af81f1249964e55ef3c6e59bbc6
Jeffrey Dill <jeffdill2@gmail.com>
2020-03-29 17:04:05 -0400

    Rebuild User class #711

--- lib/lipseys/user.rb
+++ lib/lipseys/user.rb
@@ -1,29 +1,14 @@
 module Lipseys
-  class User < SoapClient
-
-    API_URL = 'http://184.188.80.195/API/validate.asmx?WSDL'
+  class User < Base
 
     def initialize(options = {})
       requires!(options, :username, :password)
-      @options = options
+
+      @client = Lipseys::Client.new(username: options[:username], password: options[:password])
     end
 
     def authenticated?
-      validate[:success]
-    end
-
-    def validate
-      body = { Credentials: { EmailAddress: @options[:username], Password: @options[:password] } }
-      response = soap_client(API_URL).call(:validate_dealer, message: body)
-
-      result = response.body[:validate_dealer_response][:validate_dealer_result]
-
-      {
-        success: (result[:success] == 'Y'),
-        description: result[:return_desc],
-      }
-    rescue Savon::Error => e
-      { success: false, description: e.to_s }
+      @client.access_token.present?
     end
 
   end
