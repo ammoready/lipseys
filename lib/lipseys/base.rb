7d39eb729ed82f78f82bd54517f470bfc293e68d
Jeffrey Dill <jeffdill2@gmail.com>
2020-03-31 12:39:57 -0400

    Fix collect_hash_keys method ammoready/ammoready-two-point-oh#711

--- lib/lipseys/base.rb
+++ lib/lipseys/base.rb
@@ -24,10 +24,10 @@ module Lipseys
 
     private
 
-    def collect_hash_keys(hash)
-      h.each_with_object([]) do |(k,v), keys|
+    def self.collect_hash_keys(hash)
+      hash.each_with_object([]) do |(k,v), keys|
         keys << k
-        keys.concat(collect_hash_keys(v)) if v.is_a?(Hash)
+        keys.concat(self.collect_hash_keys(v)) if v.is_a?(Hash)
       end
     end
 
