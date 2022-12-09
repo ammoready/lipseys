6a1c3a50b9b66f3d87c7662faffccf18f93e45c3
Kevin Brown <brownkm531@gmail.com>
2022-12-09 15:04:51 -0500

    add drop shipping

--- lib/lipseys/order.rb
+++ lib/lipseys/order.rb
@@ -24,32 +24,48 @@ module Lipseys
       ).freeze
     }
 
+    SUBMIT_TO_DROP_SHIP_FIREARM_ATTRS = {
+      permitted: %i( ffl po name phone delay_shipping disable_email items item_no quantity note ).freeze,
+      required:  %i( ffl po name phone items item_no quantity ).freeze
+    }
+
     ENDPOINTS = {
-      submit_to_store:     "order/apiorder".freeze,
-      submit_to_drop_ship: "order/dropship".freeze,
+      submit_to_store:             "order/apiorder".freeze,
+      submit_to_drop_ship:         "order/dropship".freeze,
+      submit_to_drop_ship_firearm: "order/dropshipfirearm".freeze,
     }
 
     def initialize(client)
       @client = client
     end
 
-    def submit_to_store(order_data)
+    def submit_order(order_data, arrangement)
+      # Using the SUBMIT_TO_STORE_ATTRS as a base check, since all requests should have at least that data
       requires!(order_data, *SUBMIT_TO_STORE_ATTRS[:required])
 
-      endpoint = ENDPOINTS[:submit_to_store]
+      case arrangement
+      when :transfer_to_3rd_party
+        requires!(order_data, *SUBMIT_TO_DROP_SHIP_FIREARM_ATTRS[:required])
+
+        endpoint = ENDPOINTS[:submit_to_drop_ship_firearm]
+        order_data = standardize_body_data(order_data, SUBMIT_TO_DROP_SHIP_FIREARM_ATTRS[:permitted])
+      when :drop_ship_to_customer
+        requires!(order_data, *SUBMIT_TO_DROP_SHIP_ATTRS[:required])
+
+        endpoint = ENDPOINTS[:submit_to_drop_ship]
+        order_data = standardize_body_data(order_data, SUBMIT_TO_DROP_SHIP_ATTRS[:permitted])
+      else
+        endpoint = ENDPOINTS[:submit_to_store]
+        order_data = standardize_body_data(order_data, SUBMIT_TO_STORE_ATTRS[:permitted])
+      end
+
       headers = [
         *auth_header(@client.access_token),
         *content_type_header('application/json'),
       ].to_h
-
-      order_data = standardize_body_data(order_data, SUBMIT_TO_STORE_ATTRS[:permitted])
-
+      
       post_request(endpoint, order_data, headers)
-    end
 
-    def submit_to_drop_ship(order_data)
-      # NOTE: Will build this out as time permits.
-      false
     end
 
   end
